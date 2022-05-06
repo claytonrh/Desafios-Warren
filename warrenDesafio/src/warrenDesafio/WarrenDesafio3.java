9477074beeeb401bbf019b9648c5370734ca4009
Clayton Salvador <43305715+claytonrh@users.noreply.github.com>
2022-05-06 12:50:55 -0300

    Update WarrenDesafio3.java

--- warrenDesafio/src/warrenDesafio/WarrenDesafio3.java
+++ warrenDesafio/src/warrenDesafio/WarrenDesafio3.java
@@ -1,24 +1,26 @@
-package warrenDesafio;
+package DesafioWarren;
+import java.util.Arrays;
+import java.util.List;
 
 public class WarrenDesafio3 {
 	
-public static void main(String[] args) {		
-		
-	int n = 10; 
-	int[] v = { 2, 4, 5, 6, 9, 10 };
+public static void main(String[] args) {		       
+	    
+	 int n = 10;
+	    
+	    List<Integer> lista = Arrays.asList(2,3,4);   
 
-	for ( int i = 0; i < v.length; i++ ) {
-	    for ( int j = i + 1; j < v.length; j++ ) {
-	        for ( int k = j + 1; k < v.length; k++ ) {
-	            if ( v[i] + v[j] + v[k] == n ) {         	           
-         	            	
-	            }	
-	            int soma = i+j+k;
-            	if(soma==n) 		         
-            	System.out.printf("%d + %d + %d = %d \n",i,j,k, soma);
-	        }		        
-	    }		    
-	}		
-}	
+	    
+	    for(int a = lista.get(0); a <= lista.get(2); a++) { 
+	        for(int b = lista.get(1); b <= lista.get(2); b++) { 
+	            for(int c = lista.get(2); c <= lista.get(2); c++) {                 
+
+	    int soma = a+b+c;
+	    if(soma==n) 
+	    System.out.printf("%d + %d + %d = %d \n",a,b,c, soma);           
+	    }
+	}
+}
+}
 }
 
